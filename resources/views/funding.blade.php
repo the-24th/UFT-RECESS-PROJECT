cdc456240311e49bb71d85b96912e121ac2f8754
TayebwaCrispus <golderalikaya@gmail.com>
2019-07-23 15:14:29 +0300

    modifying graphs

--- resources/views/funding.blade.php
+++ resources/views/funding.blade.php
@@ -10,15 +10,16 @@
   <script src="{{asset('//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js')}}"></script>
   <script src="{{asset('//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js')}}"></script>
   <script src="{{asset('//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js')}}"></script>
-  
+
 </head>
 <body>
 	<br /><br />
-	<div class="container" style=="width:900px">
-	<h2 align="center"><u>BAR GRAPH SHOWING VARIATIONS IN FUNDING PER MONTH.</u></h2>
+    <div class="container" style="width:900px">
+
+	<h2 style="align:center"><u>BAR GRAPH SHOWING VARIATIONS IN FUNDING PER MONTH.</u></h2>
    <br /><br />
    <div id="chart"></div>
-		
+
 	</div>
 
 
